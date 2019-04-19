c1861ea08281e465dc88cfbd4480ab8277c4d9e2
Dzianis Dziarkach <Dzianis_Dziarkach@epam.com>
2019-04-20 01:20:29 +0300

    implemented remove approach

---
+++ Linq2ShadowTests/DatabaseContextTests/DatabaseContextTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Linq2Shadow;
+using NUnit.Framework;
+
+namespace Linq2ShadowTests.DatabaseContextTests
+{
+    [TestFixture]
+    public partial class DatabaseContextTests : TestBase
+    {
+        private DatabaseContext _sut;
+
+        protected override Task BeforeEach()
+        {
+            DbConfig.ReInitDatabase();
+            _sut = new DatabaseContext(DbConfig.CreateConnectionAndOpen);
+            return base.BeforeEach();
+        }
+
+        protected override Task AfterEach()
+        {
+            _sut.Dispose();
+            return base.AfterEach();
+        }
+    }
+}
